7ff5c2ba72f3874e246252198fd97953b9974393
Rhonnie Al <rhonnieallan@protonmail.com>
2023-03-11 10:44:42 +0200

    Validation: add data validation & error handling

--- models/task.js
+++ models/task.js
@@ -3,8 +3,17 @@ const mongoose = require("mongoose");
 // Set up structure of the documents in the DB collection
 // For this task-manager-project, name(string) & completed property(bool)
 const taskSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
+  // Basic data validation
+  name: {
+    type: String,
+    required: [true, "Please add a name for the task"],
+    trim: true,
+    maxlength: [20, "Name cannot be more than 20 characters"],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 // export the mongoose model (accepts name and schema)
